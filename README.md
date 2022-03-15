6e4f5a8b73dbbfafdec0850dcdd77af270662b37
shoppingzh <1049262057@qq.com>
2022-03-15 17:49:13 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tongpo
-乐天工具库
+
+> tongpo取自“苏东坡”，又称乐天工具库，旨在发现更多的业务共性，抽取出稳定接口的工具集，适应更多业务场景，帮助开发者快速解决问题。
+
+📃文档：[https://shoppingzh.gitee.io/tongpo/](https://shoppingzh.gitee.io/tongpo/)
+
+
+
--- src/array.js
+++ src/array.js
@@ -10,12 +10,12 @@ function isOutOfBound(arr, index) {
  * @param {Number} offset 移动偏移量(负值为向前移动，正值为向后移动)
  */
 export function move(arr, index, offset) {
-  if (!Array.isArray(arr)) return
-  if (isOutOfBound(arr, index)) throw new Error(`index: ${index} bounds: [0, ${arr.length}]`)
-  const dstIndex = index + offset
-  if (isOutOfBound(arr, dstIndex)) throw new Error(`destination index: ${dstIndex} bounds:[0, ${arr.length}]`)
-  const item = arr[index]
-  arr.splice(index, 1)
-  arr.splice(dstIndex, 0, item)
+  // if (!Array.isArray(arr)) return
+  // if (isOutOfBound(arr, index)) throw new Error(`index: ${index} bounds: [0, ${arr.length}]`)
+  // const dstIndex = index + offset
+  // if (isOutOfBound(arr, dstIndex)) throw new Error(`destination index: ${dstIndex} bounds:[0, ${arr.length}]`)
+  // const item = arr[index]
+  // arr.splice(index, 1)
+  // arr.splice(dstIndex, 0, item)
   return arr
 }
